2cbb25fa2cc2cb767893257afc04701be021360f
arytmw <54942957+arytmw@users.noreply.github.com>
2024-03-23 08:53:18 +0530

    Create create-certs.sh

---
+++ create-certs.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+openssl genrsa -out app.key 2048
+openssl req -new -key app.key -out app.csr -config app.config
+openssl req -x509 -sha256 -nodes -new -key app.key -out app.crt -config app.config
+openssl x509 -sha256 -CAcreateserial -req -days 365 -in app.csr -extfile app.config -CA app.crt -CAkey app.key -out main.crt
