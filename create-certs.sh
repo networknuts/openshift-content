ef6fa5231f84ddf363097241ac7d727563ef34ea
arytmw <54942957+arytmw@users.noreply.github.com>
2024-03-31 21:59:32 +0530

    Update create-certs.sh

--- create-certs.sh
+++ create-certs.sh
@@ -4,3 +4,6 @@ openssl genrsa -out app.key 2048
 openssl req -new -key app.key -out app.csr -config app.config
 openssl req -x509 -sha256 -nodes -new -key app.key -out app.crt -config app.config
 openssl x509 -sha256 -CAcreateserial -req -days 365 -in app.csr -extfile app.config -CA app.crt -CAkey app.key -out main.crt
+
+echo "Run the following command to create your Openshift TLS Secret:"
+echo "oc create secret tls <secret-name> --cert main.crt --key app.key"
